1c11eb11f53b8ee79c4f3d5c5e8add7589ef8fd7
SPluzh <satrap.serg@gmail.com>
2026-08-13 15:10:23 +0300

    Add diagnostic metrics to getFacesFromVerticesFast logging

--- src/sculpt/SculptEngine.cpp
+++ src/sculpt/SculptEngine.cpp
@@ -420,6 +420,9 @@ uint32_t getFacesFromVerticesFast(
     auto tEpochDone = std::chrono::high_resolution_clock::now();
 
     uint32_t acc = 0;
+    uint64_t totalTraversals = 0;
+    uint32_t duplicateHits = 0;
+
     for (uint32_t i = 0; i < nbIVerts; ++i) {
         if (i + 4 < nbIVerts) {
             uint32_t nextId = iVerts[i + 4];
@@ -429,6 +432,8 @@ uint32_t getFacesFromVerticesFast(
         uint32_t idVert = iVerts[i];
         uint32_t start = vrfStartCount[idVert * 2];
         uint32_t count = vrfStartCount[idVert * 2 + 1];
+        totalTraversals += count;
+
         for (uint32_t j = start; j < start + count; ++j) {
             if (j + 4 < start + count) {
                 __builtin_prefetch(&tagFlags[vertRingFace[j + 4]], 1, 2);
@@ -437,6 +442,8 @@ uint32_t getFacesFromVerticesFast(
             if (tagFlags[iFace] != epoch) {
                 tagFlags[iFace] = epoch;
                 outIFaces[acc++] = iFace;
+            } else {
+                duplicateHits++;
             }
         }
     }
@@ -446,8 +453,8 @@ uint32_t getFacesFromVerticesFast(
     double epoch_ms = std::chrono::duration<double, std::milli>(tEpochDone - t0).count();
     double loop_ms = std::chrono::duration<double, std::milli>(t2 - tEpochDone).count();
     if (total_ms > 0.5) {
-        sculpt_log("[C++ getFacesFromVerticesFast] %u verts -> %u faces | total: %.2fms (loop: %.2fms, epoch reset: %s, reset time: %.2fms)\n",
-                  nbIVerts, acc, total_ms, loop_ms, resetEpoch ? "true" : "false", epoch_ms);
+        sculpt_log("[C++ getFacesFromVerticesFast] %u verts -> %u unique faces | traversals: %llu (avg %.2f/vert), dup face hits: %u | total: %.2fms (loop: %.2fms, epoch reset: %s, reset time: %.2fms)\n",
+                  nbIVerts, acc, (unsigned long long)totalTraversals, nbIVerts > 0 ? (double)totalTraversals / nbIVerts : 0.0, duplicateHits, total_ms, loop_ms, resetEpoch ? "true" : "false", epoch_ms);
     }
 
     return acc;
