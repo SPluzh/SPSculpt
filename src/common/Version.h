c6c53e0afc65123e79de17ec95ef7f51a7b9a0b2
SPluzh <satrap.serg@gmail.com>
2026-08-15 19:02:59 +0300

    Bump version to 0.2.5

--- src/common/Version.h
+++ src/common/Version.h
@@ -3,9 +3,9 @@
 namespace Version {
     constexpr int MAJOR = 0;
     constexpr int MINOR = 2;
-    constexpr int PATCH = 2;
+    constexpr int PATCH = 5;
     constexpr const char* SUFFIX = "";
-    constexpr const char* STRING = "0.2.2";
+    constexpr const char* STRING = "0.2.5";
     constexpr const char* APP_NAME = "SPSculpt";
 }
 
